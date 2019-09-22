6eb777d4f462684239aa6749701abd964616ddab
Bruno <xuguangzhi2003@126.com>
2019-09-23 06:24:41 +0800

    unicode_csv_reader

--- mailman-send.py
+++ mailman-send.py
@@ -60,16 +60,21 @@ def parse_args():
         g_test = args['test']
 
 
-def unicode_csv_reader(unicode_csv_data, dialect=csv.excel, **kwargs):
-    # csv.py doesn't do Unicode; encode temporarily as UTF-8:
-    csv_reader = csv.reader(utf_8_encoder(unicode_csv_data),
-                            dialect=dialect, **kwargs)
+# def unicode_csv_reader(unicode_csv_data, dialect=csv.excel, **kwargs):
+#     # csv.py doesn't do Unicode; encode temporarily as UTF-8:
+#     csv_reader = csv.reader(utf_8_encoder(unicode_csv_data),
+#                             dialect=dialect, **kwargs)
+#     for row in csv_reader:
+#         # decode UTF-8 back to Unicode, cell by cell:
+#         yield [unicode(cell, 'utf-8') for cell in row]
+# def utf_8_encoder(unicode_csv_data):
+#     for line in unicode_csv_data:
+#         yield line.encode('utf-8')
+
+def unicode_csv_reader(utf8_data, dialect=csv.excel, **kwargs):
+    csv_reader = csv.reader(utf8_data, dialect=dialect, **kwargs)
     for row in csv_reader:
-        # decode UTF-8 back to Unicode, cell by cell:
         yield [unicode(cell, 'utf-8') for cell in row]
-def utf_8_encoder(unicode_csv_data):
-    for line in unicode_csv_data:
-        yield line.encode('utf-8')
 
 
 # 发邮件
