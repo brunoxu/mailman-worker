f14669df758a4cf7a06610a13c9bec904531cc0c
Bruno <xuguangzhi2003@126.com>
2019-09-23 05:21:17 +0800

    csv read utf-8

--- mailman-feedback.py
+++ mailman-feedback.py
@@ -60,6 +60,18 @@ def parse_args():
     #     g_test = args['test']
 
 
+def unicode_csv_reader(unicode_csv_data, dialect=csv.excel, **kwargs):
+    # csv.py doesn't do Unicode; encode temporarily as UTF-8:
+    csv_reader = csv.reader(utf_8_encoder(unicode_csv_data),
+                            dialect=dialect, **kwargs)
+    for row in csv_reader:
+        # decode UTF-8 back to Unicode, cell by cell:
+        yield [unicode(cell, 'utf-8') for cell in row]
+def utf_8_encoder(unicode_csv_data):
+    for line in unicode_csv_data:
+        yield line.encode('utf-8')
+
+
 # 发反馈
 def start_feedback():
     global g_workdir, g_logdir, g_test\
@@ -69,8 +81,7 @@ def start_feedback():
 
     interval_arr = g_task_config['interval'].split(',')
 
-    import io
-    with io.open(g_file_mail_result, mode='r', encoding="utf-8") as fp:
+    with open(g_file_mail_result, mode='r') as fp:
         csv_reader = csv.reader(fp, delimiter=',')
         row_count = 0
         conn = httplib.HTTPSConnection('mailman.sme.wang')
